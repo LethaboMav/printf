fb299e249767e3895fda7cf34c19432ce34e8156
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-27 15:06:55 +0000

    Update header file with newly added function prototypes

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
+int print_c(va_list l);
+int print_s(va_list l);
+int print_p(void);
 
 #endif /* MAIN_H */
