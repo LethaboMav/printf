c271c886978376d7106e34d449b975cceb8f55c6
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-28 14:01:18 +0000

    Correct coding style

--- 1-print_i_d.c
+++ 1-print_i_d.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_c - prints a character from a variable list of arguments.
- * @l: the variable list.
+ * print_i_d - prints an integer from a variable list of arguments.
+ * @y: the variable list.
  *
- * Return: the number of characters printed.
+ * Return: the length of the integer printed.
  */
 int print_i_d(va_list y)
 {
-        int ret;
+	int ret;
 	char buff[12];
 
 	ret = sprintf(buff, "%d", va_arg(y, int));
 
 	if (ret < 0)
 		return (-1);
-	write(1, buff, strlen(buff));	
+	write(1, buff, strlen(buff));
 
 	return (ret);
 }
