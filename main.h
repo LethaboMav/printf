c228c1a7c649098b23ffbe7a5c66fe023acc58df
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-01 16:46:21 +0000

    Update header file

--- main.h
+++ main.h
@@ -11,5 +11,8 @@ int print_c(va_list l);
 int print_s(va_list l);
 int print_p(void);
 int print_i_d(va_list y);
-
+int _strlen(char *s);
+void convertb(unsigned int num, char *binary);
+void rev_string(char *s);
+int print_b(va_list l);
 #endif /* MAIN_H */
